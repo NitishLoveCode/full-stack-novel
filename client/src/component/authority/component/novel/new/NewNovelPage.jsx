ebbee94e0715c91981afaeabbb509df1b57ccaee
nitishlovecode <nitishlovecode@gmail.com>
2023-09-23 12:49:23 -0700

    new post page changes push

--- client/src/component/authority/component/novel/new/NewNovelPage.jsx
+++ client/src/component/authority/component/novel/new/NewNovelPage.jsx
@@ -1,26 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import {BsPlusSquareDotted} from "react-icons/bs"
+
 
 export default function NewNovelPage() {
+    const [content,setcontent]=useState(null)
   return (
     <>
     <div className='flex'>
-        <div className='w-[60vw]'>
-            <div className='border-[1px] overflow-hidden border-gray-600 h-10 rounded-md flex items-center'>
-                <input className='w-full h-full pl-2 outline-primary' type="text" name="title" placeholder='Title'/>
+        <div className='w-[57vw]'>
+            <div>
+                <input className='w-full h-10 outline-primary bg-gray-200 rounded-xl pl-3' type="text" name="title" placeholder='Title'/>
             </div>
-            <div className='mt-2 border-[1px] overflow-hidden border-gray-600 h-10 rounded-md flex items-center'>
-                <input className='w-full h-full pl-2 outline-primary' type="text" name="url" placeholder='URL'/>
+            <div>
+                <input className='w-full h-10 outline-primary bg-gray-200 rounded-xl pl-3 mt-2' type="text" name="url" placeholder='URL'/>
             </div>
-            <div className='border-[1px] border-gray-600 rounded-md overflow-hidden mt-4 h-[70vh]'>
-                <textarea className='w-full h-full p-2 outline-primary' name="content" placeholder='Content'></textarea>
+            <div className='mt-4 h-[72vh]'>
+                {/* <textarea className='w-full rounded-md h-full outline-primary outline-4 bg-gray-200 p-3' name="content" placeholder='Content'></textarea> */}
+                <div style={{height: 430}}>
+                   <ReactQuill className='h-full' theme="snow" value={content} onChange={setcontent} />
+                </div>
             </div>
         </div>
 
 
         <div className='w-[30vw] ml-4'>
             <div className='flex items-center justify-between'>
-                <button className='bg-primary p-3 rounded-md text-white px-14'>Save Draft</button>
-                <button className='bg-primary p-3 rounded-md text-white px-14'>Publish</button>
+                <button className='bg-primary p-3 rounded-md active:scale-95 text-white px-14'>Publish</button>
+                <button className='bg-gray-500 p-3 rounded-md active:scale-95 text-white px-14 '>Save Draft</button>
+            </div>
+
+            {/* -------------discription----------- */}
+            <div>
+                <textarea className='w-full h-28 bg-gray-200 outline-primary mt-4 p-2 rounded-md' name="description" placeholder='Description' ></textarea>
+            </div>
+
+            <div className='flex justify-between mt-5'>
+                <button className='bg-primary p-2 px-3 text-white rounded-md active:scale-95'>Select novel</button>
+                <button className='bg-gray-300 p-2 px-3 rounded-md active:scale-95'>Create new</button>
+                <button className='bg-gray-300 p-2 px-3 rounded-md'>Last page: 298</button>
+            </div>
+
+
+            <div className='mt-8'>
+                <div className='flex justify-between'>
+                    <div className='bg-gray-200 active:scale-95 h-36 rounded-md flex items-center justify-center w-[55%]'>
+                        <BsPlusSquareDotted className="text-4xl"/>
+                    </div>
+                    <div className='flex flex-col justify-between'>
+                        <button className='bg-primary p-2 rounded-md text-white py-3 active:scale-95'>Gallery</button>
+                        <button className='bg-gray-300 p-2 rounded-md text-gray-900 py-3'>Current Page: 299</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <input className='w-full h-12 px-2 outline-primary rounded-md bg-gray-200' type="text" name="tag" placeholder='tag1, tag2'/>
+            </div>
+
+            <div className='mt-8 p-2 active:scale-95 flex items-center justify-center text-lg text-white rounded-lg bg-gradient-to-r from-purple-500 to-pink-500'>
+                <button>Delete</button>
             </div>
 
         </div>
